28f3f7167e620ea059dadd8c7535664782ef3cb0
jacksonb2021 <jacksonburns2021@gmail.com>
2024-08-06 12:11:33 -0700

    update wording on percentages
    Took 54 seconds

--- script.js
+++ script.js
@@ -13,32 +13,33 @@ function calculateFees() {
 	if (x < 2000000) {
 		let monthlyFee = x / 1200;
 		totalFee = monthlyFee;
-		// No special output for monthly percentage in this case
+		result+="Up to 2M:".padEnd(padding)+monthlyFee.toFixed(2).padStart(10)+'\n'
+		result+="1% annually, .0833%/month"+'\n'
 	} else if (x < 10000000) {
 		let monthlyFee = (x * 0.004) / 12;
 		result += "Retainer:".padEnd(padding) + "1,000.00".padStart(10) + "\n";
 		result += "Up to 10M:".padEnd(padding) + monthlyFee.toFixed(2).padStart(10) + "\n";
-		result += ".4% annually, .0333PPM\n";
+		result += ".4% annually, .0333%/month\n";
 		totalFee = retainer + monthlyFee;
 	} else if (x < 20000000) {
 		result += "Retainer:".padEnd(padding) + "1,000.00".padStart(10) + "\n";
 		result += "Up to 10M:".padEnd(padding) + tenM.toFixed(2).padStart(10) + "\n";
-		result += ".4% annually, .0333PPM\n";
+		result += ".4% annually, .0333%/month\n";
 		let monthlyFee = ((x - 10000000) * 0.003) / 12;
 		result += "From 10M to 20M:".padEnd(padding) + monthlyFee.toFixed(2).padStart(10) + "\n";
-		result += ".3% annually, .025PPM\n";
+		result += ".3% annually, .025%/month\n";
 		totalFee = retainer + tenM + monthlyFee;
 	} else {
 		result += "Retainer:".padEnd(padding) + "1,000.00".padStart(10) + "\n";
 		result += "Up to 10M:".padEnd(padding) + tenM.toFixed(2).padStart(10) + "\n";
-		result += ".4% annually, .0333PPM\n";
+		result += ".4% annually, .0333%/month\n";
 
 		result += "From 10M to 20M:".padEnd(padding) + twentyM.toFixed(2).padStart(10) + "\n";
-		result += ".3% annually, .025PPM\n";
+		result += ".3% annually, .025%/month\n";
 
 		let monthlyFee = ((x - 20000000) * 0.0025) / 12;
 		result += "Above 20M:".padEnd(padding) + monthlyFee.toFixed(2).padStart(10) + "\n";
-		result += ".25% annually, .0208PPM\n";
+		result += ".25% annually, .0208%/month\n";
 
 		totalFee = retainer + tenM + twentyM + monthlyFee;
 	}
